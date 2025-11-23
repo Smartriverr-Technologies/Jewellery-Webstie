76ea406cf11fbf7e4f699769fcfedff683d91295
AnuragSaxena1999 <anurag1705094@gmail.com>
2025-11-23 21:09:46 +0530

    filter 2

--- Frontend-customer/src/components/ShopLayout.jsx
+++ Frontend-customer/src/components/ShopLayout.jsx
@@ -1,57 +1,3 @@
-// import React from 'react';
-// import { Grid, Container, Box } from '@mui/material';
-// import FilterSidebar from './FilterSidebar';
-
-// const ShopLayout = ({ children, onFilterChange }) => {
-//   return (
-//     <Container maxWidth="xl" sx={{ py: 1 }}>
-//       <Grid container spacing={3} alignItems="flex-start">
-        
-//         {/* --- Left: Sticky Filter Sidebar --- */}
-//         <Grid item xs={12} md={3} sx={{
-//           position: 'relative',
-//           height: 'fit-content',
-//           // border: '1px solid black',
-//           width: '23%',
-//           float: 'left',
-//         }}>
-//           <FilterSidebar onFilterChange={onFilterChange} />
-//         </Grid>
-
-//         {/* --- Right: Scrollable Products Section --- */}
-//         <Grid 
-//           item 
-//           xs={12} 
-//           md={9} 
-//           sx={{
-//             maxHeight: 'calc(100vh - 100px)', // adjust according to navbar height
-//             overflowY: 'auto',
-//             pr: 1,
-//             '&::-webkit-scrollbar': { width: '6px' },
-//             '&::-webkit-scrollbar-thumb': { background: '#B8860B', borderRadius: '3px' },
-//             // border: '1px solid black',
-//             width: '75%',
-//             float: 'right',
-//             // display: 'flex',
-//             // alignItems: 'center',
-//             jsustifyContent: 'center',
-//             alignContent: 'center',
-//             px: 4,
-//             marginTop: 2,
-//           }}
-//         >
-//           <Box>
-//             {children}
-//           </Box>
-//         </Grid>
-
-//       </Grid>
-//     </Container>
-//   );
-// };
-
-// export default ShopLayout;
-
 import React from "react";
 import { Grid, Container, Box, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
@@ -59,17 +5,16 @@ import FilterSidebar from "./FilterSidebar";
 
 const ShopLayout = ({ children, onFilterChange }) => {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md")); // mobile = < md screens
+  const isMobile = useMediaQuery(theme.breakpoints.down("md")); // detects screen < md
 
   return (
     <Container maxWidth="xl" sx={{ py: 1 }}>
       <Grid container spacing={3} alignItems="flex-start">
-        
-        {/* ---------- Filter Sidebar - Hide Completely on Mobile ---------- */}
+
+        {/* ---- Only render this grid when not mobile ---- */}
         {!isMobile && (
           <Grid
             item
-            xs={12}
             md={3}
             sx={{
               position: "relative",
@@ -82,7 +27,7 @@ const ShopLayout = ({ children, onFilterChange }) => {
           </Grid>
         )}
 
-        {/* ---------- Products Section (Full width on mobile) ---------- */}
+        {/* ---- Product Section always visible ---- */}
         <Grid
           item
           xs={12}
@@ -95,20 +40,15 @@ const ShopLayout = ({ children, onFilterChange }) => {
             "&::-webkit-scrollbar-thumb": { background: "#B8860B", borderRadius: "3px" },
             width: isMobile ? "100%" : "75%",
             float: "right",
-            justifyContent: "center",
-            px: 2,
+            px: isMobile ? 0 : 4,
             mt: 2,
           }}
         >
-          <Box>
-            {children}
-          </Box>
+          <Box>{children}</Box>
         </Grid>
-
       </Grid>
     </Container>
   );
 };
 
 export default ShopLayout;
-
